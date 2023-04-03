e05d464be2676a0ae81cea19aee546a7cb0a1d83
Eya Chebbi <eya.chebbi1234@gmail.com>
2023-04-03 12:15:46 +0100

    Update SignUp.js

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,6 +1,6 @@
 import React, { Component, useState} from 'react';
-import { Alert, ActivityIndicator, Keyboard, KeyboardAvoidingView, StyleSheet, Button, TextInput, Text } from 'react-native';
-import { Block } from '../components';
+import { Alert, ActivityIndicator, Keyboard, KeyboardAvoidingView, StyleSheet } from 'react-native';
+import { Block, Input, Button, Text } from '../components';
 import { theme } from '../constants';
 import {axios} from 'axios';
 const API_URL = 'http://localhost:3000';
@@ -17,49 +17,49 @@ export default function SignUp({ navigation })  {
       
     
       
-      Keyboard.dismiss();
-      setLoading(true);
+    //   Keyboard.dismiss();
+    //   setLoading(true);
 
-      // check with backend API or with some static data
-      if (!email) errors.push('email');
-      if (!username) errors.push('username');
-      if (!password) errors.push('password');
+    //   // check with backend API or with some static data
+    //   if (!email) errors.push('email');
+    //   if (!username) errors.push('username');
+    //   if (!password) errors.push('password');
 
-      setErrors(errors);
-      setLoading(false);
+    //   setErrors(errors);
+    //   setLoading(false);
 
-      axios.post(`${API_URL}/register`, { email, username, password })
-      .then(response => {
-        console.log(response.data.token);
-        Alert.alert(
-          'Success!',
-          'Your account has been created',
-          [
-            {
-              text: 'Continue', onPress: () => {
-                navigation.navigate('Browse')
-              }
-            }
-          ],
-          { cancelable: false }
-        )
-      })
-      .catch(error => {
-        setErrors(error.response.data.errors);
-        console.log(error.response.data);
-      })
-      .finally(() => setLoading(false));
+    //   axios.post(`${API_URL}/register`, { email, username, password })
+    //   .then(response => {
+    //     console.log(response.data.token);
+    //     Alert.alert(
+    //       'Success!',
+    //       'Your account has been created',
+    //       [
+    //         {
+    //           text: 'Continue', onPress: () => {
+    //             navigation.navigate('Browse')
+    //           }
+    //         }
+    //       ],
+    //       { cancelable: false }
+    //     )
+    //   })
+    //   .catch(error => {
+    //     setErrors(error.response.data.errors);
+    //     console.log(error.response.data);
+    //   })
+    //   .finally(() => setLoading(false));
 
-    }
+     }
      const hasErrors = key => errors.includes(key) ? styles.hasErrors : null;
 
 
   return(
       <KeyboardAvoidingView style={styles.signup} behavior="padding">
         <Block padding={[0, theme.sizes.base * 2]}>
-          <Text h1 bold>Sign Up</Text>
+          {/* <Text h1 bold>Sign Up</Text> */}
           <Block middle>
-            <TextInput
+            <Input
               email
               label="Email"
               error={hasErrors('email')}
@@ -67,14 +67,14 @@ export default function SignUp({ navigation })  {
               defaultValue={email}
               onChangeText={text => setEmail(text)}
             />
-            <TextInput
+            <Input
               label="Username"
               error={hasErrors('username')}
               style={[styles.input, hasErrors('username')]}
               defaultValue={username}
               onChangeText={text => setUsername(text)}
             />
-            <TextInput
+            <Input
               secure
               label="Password"
               error={hasErrors('password')}
@@ -82,7 +82,7 @@ export default function SignUp({ navigation })  {
               defaultValue={password}
               onChangeText={text => setPassword(text)}
             />
-             <Button title="Sign Up" onPress={() => handleSignUp()}>
+             <Button gradient title="Sign Up" onPress={() => handleSignUp()}>
               {loading ?
                 <ActivityIndicator size="small" color="white"/> :
                 <Text bold white center>Sign Up</Text>
